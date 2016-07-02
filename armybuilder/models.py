8f0330ba6733c798403db7f4871f8489ec48d86c
Torkel Berli <torkelbe@gmail.com>
2016-07-02 20:22:57 +0200

    Remove database models and switch to postgres for deploying.

--- armybuilder/models.py
+++ armybuilder/models.py
@@ -2,10 +2,3 @@ from __future__ import unicode_literals
 from django.db import models
 # Create your models here.
 
-class Army(models.Model):
-    name = models.CharField(max_length=30)
-    alignment = models.CharField(max_length=10)
-
-class Unit(models.Model):
-    army = models.ForeignKey(Army, on_delete=models.CASCADE)
-    unit_type = models.CharField(max_length=20)
