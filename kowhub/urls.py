33a48dd787d3f40c3c841ad59b004bce3b4e9c88
Torkel Berli <torkelbe@gmail.com>
2016-07-02 17:21:22 -0400

    Redirect home view to armybuilder app.

--- kowhub/urls.py
+++ kowhub/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.views.generic import RedirectView
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^armybuilder/', include('armybuilder.urls')),
+    url(r'^$', RedirectView.as_view(url='armybuilder')),
 ]
