2acca0f6405fd5fcf89b03c28619c8a46bfb6d70
Torkel Berli <torkelbe@gmail.com>
2017-10-03 20:40:23 +0900

    Adjust name of temporary data generator files.

--- armybuilder/kowdatagen/data_locations.py
+++ armybuilder/kowdatagen/data_locations.py
@@ -7,9 +7,12 @@ import os
 class DataLocations:
     def __init__(self):
         base_dir = os.path.dirname(os.path.realpath(__file__))
+
         self.numbers = _DLType(base_dir+"/../../../kowsources/army_data.numbers", base_dir+"/../../../kowsources/rules_data.numbers")
-        if not os.path.isdir(base_dir+"/csv"): os.mkdir(base_dir+"/csv")
-        self.csv = _DLType(base_dir+"/csv/armies", base_dir+"/csv/rules")
+
+        if not os.path.isdir(base_dir+"/temp"): os.mkdir(base_dir+"/temp")
+        self.csv = _DLType(base_dir+"/temp/armies", base_dir+"/temp/rules")
+
         if not os.path.isdir(base_dir+"/../data"): os.mkdir(base_dir+"/../data")
         self.json = base_dir+"/../data/kowdata.json"
 
