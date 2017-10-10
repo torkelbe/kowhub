594d2c2c1bf0958f98c0e152f57a872f9ff71d26
Torkel Berli <torkelbe@gmail.com>
2017-10-10 12:09:39 +0900

    Serve common static files from 'BASE_DIR + static/'.

--- kowhub/settings/base.py
+++ kowhub/settings/base.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/1.9/ref/settings/
 
 import os
 
+# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
@@ -120,4 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, 'static/'), # common static files
+]
 
