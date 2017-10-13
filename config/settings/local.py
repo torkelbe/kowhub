bf84c19850937f122002593606fb55ecf8c4ff12
Torkel Berli <torkelbe@gmail.com>
2017-10-13 13:24:50 +0900

    Update django local settings to import from base settings and provide relative path to its db.sqlite3 file.

--- config/settings/local.py
+++ config/settings/local.py
@@ -1,17 +1,17 @@
-# SECURITY WARNING: don't run with debug turned on in production!
+from .base import *
+
 DEBUG = True
+
 ADMIN_ENABLED = True
 
 COMPRESS_ENABLED = False
 
 ALLOWED_HOSTS = []
 
-# Database
-# https://docs.djangoproject.com/en/1.9/ref/settings/#databases
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': '/absolute/path/to/db.sqlite3'),
+        'NAME': join(PROJECT_ROOT, 'db.sqlite3'),
         'USER': '',         # not used with sqlite
         'PASSWORD': '',     # not used with sqlite
         'HOST': '',         # not used with sqlite
@@ -21,6 +21,4 @@ DATABASES = {
 
 STATIC_ROOT = ""
 
-# SECURITY WARNING: keep the secret key used in production secret!
-# CHANGE THIS! THIS IS PUBLIC AS IT'S IN SOURCE CONTROL
 SECRET_KEY = '6n7d_c302kd^&gty^zfeh#tkz5_yma6(z(h8uh@sz3!va^*$p('
