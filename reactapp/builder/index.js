3f05a2939ba019ccd8c8edf642a4abe4c51656c3
Torkel Berli <torkelbe@gmail.com>
2017-10-14 17:15:02 +0900

    Begin 'hello world' react app.

---
+++ reactapp/builder/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ArmybuilderApp from './js/main'
+
+ReactDOM.render(
+    <ArmybuilderApp />,
+    window.react_mount
+);
---
+++ reactapp/builder/js/main.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class ArmybuilderApp extends Component {
+    render() {
+        return (
+            <p>HELLO THIS IS REACT APP!</p>
+        );
+    }
+}
+
+export default ArmybuilderApp;
