deef0e0ce78a45cbd1624f8ed911ef483fdd939d
Torkel Berli <torkelbe@gmail.com>
2017-10-29 16:09:43 +0900

    Configure Webpack to optimize and inline SVG.

--- reactapp/webpack.base.config.js
+++ reactapp/webpack.base.config.js
@@ -24,7 +24,7 @@ module.exports = {
                 ]
             },
             {
-                test: /\.(png|svg|jpg)$/,
+                test: /\.(png|jpg)$/,
                 use: [
                     'file-loader'
                 ]
@@ -33,6 +33,23 @@ module.exports = {
                 test: /\.(woff|woff2|eot|ttf|otf)$/,
                 use: [
                     'file-loader'
+                ]
+            },
+            {
+                test: /\.svg$/,
+                use: [
+                    {
+                        loader: 'svg-react-loader',    // Convert to React component
+                        query: {
+                        }
+                    },
+                    {
+                        loader: 'svgo-loader',    // Optimize SVG
+                        options: {
+                            plugins: [
+                            ]
+                        }
+                    }
                 ]
             }
         ]
