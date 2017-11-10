4a6eda0001dbd4d7b30f63b47673cd841a299a0e
Torkel Berli <torkelbe@gmail.com>
2017-11-10 13:20:29 +0900

    Expand the set of iconbutton types.

--- frontend/src/builder/components/iconbutton.jsx
+++ frontend/src/builder/components/iconbutton.jsx
@@ -2,16 +2,22 @@ import React from 'react';
 
 import NewIcon from 'svg/temp/new-icon.svg';
 import DeleteIcon from 'svg/temp/delete-icon.svg';
+import PrintIcon from 'svg/temp/print-icon.svg';
+import InfoIcon from 'svg/temp/info-icon.svg';
+import SettingsIcon from 'svg/temp/settings-icon.svg';
 
 const buttonType = {
     new: NewIcon,
     delete: DeleteIcon,
+    print: PrintIcon,
+    info: InfoIcon,
+    settings: SettingsIcon,
 }
 
-export default function IconButton(props) {
-    const Svg = buttonType[props.type];
+export default function IconButton({type, onClick}) {
+    const Svg = buttonType[type];
     return (
-        <div className="kb-icon-button" onClick={(e) => props.onClick(e)} >
+        <div className="kb-icon-button" onClick={(e) => onClick(e)} >
             <Svg />
         </div>
     );
