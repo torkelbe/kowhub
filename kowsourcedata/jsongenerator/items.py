3da49f2bf7f8101fdf063153ed69ce4389676371
Torkel Berli <torkelbe@gmail.com>
2019-05-07 18:17:23 +0900

    Update number of fields for items parsing

--- kowsourcedata/jsongenerator/items.py
+++ kowsourcedata/jsongenerator/items.py
@@ -11,12 +11,13 @@ def parse_items():
                 file.readline() # throw first line
                 line = file.readline()
                 while line:
-                    name, key, pts, modifier, limitation, version, status, description = csvutils.get_elements(line);
+                    name, key, pts, modifier, limitation, version, status, description, armylist_description = csvutils.get_elements(line);
                     modifier = ""       # How the item modifies unit profile. Not yet used.
                     limitation = ""     # Limitation on who can choose the item. Not yet used.
                     version = ""        # To what ruleset does this item belong. Not yet used.
                     status = ""         # Current status of the rules for this item. Not yet used.
                     description = ""    # Rulebook description of this item
+                    armylist_description = "" # Armylist description of this item
 
                     if len(key) != 3:
                         error_logger.critical("Non-standard length(3) of item key", key, name)
