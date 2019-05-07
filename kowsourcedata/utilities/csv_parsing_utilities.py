7164e172c1bd28fad06d7adb87e7f8870ea23082
Torkel Berli <torkelbe@gmail.com>
2019-05-07 18:18:00 +0900

    Correctly parse CSV files with comma separator

--- kowsourcedata/utilities/csv_parsing_utilities.py
+++ kowsourcedata/utilities/csv_parsing_utilities.py
@@ -1,21 +1,39 @@
 #!/usr/bin/python
 
+import re
+
+SEPARATOR = ','
+ENCODED_SEPARATOR = '$$'
+CSV_COMMENTED_LINE_REGEX = r'\".*?\"'
+
 def _get_stats_obj(sp, me, ra, de, att, ne, pts):
     me = me if me=='-' else me+'+'
     ra = ra if ra=='-' else ra+'+'
     de = de+'+'
     return [sp, me, ra, de, att, ne, int(pts)]
 
+def separator_replacer(matchobj):
+    return matchobj.group(0).replace(SEPARATOR, ENCODED_SEPARATOR)
+
+def encode(line):
+    return re.sub(CSV_COMMENTED_LINE_REGEX, separator_replacer, line)
+
+def decode(text):
+    return text.replace(ENCODED_SEPARATOR, SEPARATOR).strip().strip('"')
+
+def split_line(line):
+    return line.rstrip('\r\n').split(SEPARATOR)
+
 class CsvParsingUtilities:
 
     def __init__(self, csv_separator_character=';'):
-        self.separator = csv_separator_character
+        #self.separator = csv_separator_character
+        pass
 
     def get_elements(self, line):
-        elements =  line.rstrip('\r\n').split(self.separator)
-        for e in elements:
-            e = e.strip()
-        return elements
+        line = encode(line)
+        elements = split_line(line)
+        return [decode(text) for text in elements]
 
     def read_army(self, line):
         entry, order, armykey, armyname, short_armyname, alignment, size, sp, me, ra, de, att, ne, pts, special, options, version, status = self.get_elements(line)
